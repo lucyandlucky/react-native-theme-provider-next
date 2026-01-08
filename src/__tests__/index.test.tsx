3f99b772df9b2027cd288e62d3ad2d9c671ae89b
lucyandlucky <lucykfc1@gmail.com>
2026-01-08 14:59:01 +0800

    feat: 初始化单元测试

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,39 @@
-it.todo('write a test');
+// import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import createTheming from '../provider/createTheming';
+
+describe('createTheming', () => {
+  const darkTheme = {
+    primaryColor: '#FFA72A',
+    accentColor: '#458622',
+    backgroundColor: '#504f4d',
+    textColor: '#FFC777',
+    secondaryColor: '#252525',
+  };
+
+  // const lightTheme = {
+  //   primaryColor: '#ffcaaa',
+  //   accentColor: '#45ffaa',
+  //   backgroundColor: '#aaffcf',
+  //   textColor: '#FFa7af',
+  //   secondaryColor: '#ffffff',
+  // };
+
+  const { ThemeProvider, useTheme } = createTheming(darkTheme);
+
+  it('provides theme with hook', () => {
+    const PropsChecker = (props: any) => {
+      const theme = useTheme(props.theme);
+      expect(typeof theme).toBe('object');
+      expect(theme).toEqual(darkTheme);
+      return null;
+    };
+
+    render(
+      <ThemeProvider>
+        <PropsChecker />
+      </ThemeProvider>
+    );
+  });
+});
