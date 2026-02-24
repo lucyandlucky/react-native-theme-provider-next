87db76bee5ad9a041a629fd16856c4fb6a310cf8
lucyandlucky <lucykfc1@gmail.com>
2026-02-24 10:17:08 +0800

    feat: 调整 index.d.ts

---
+++ typings/index.d.ts
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import hoistNonReactStatics = require('./hoist-non-react-statics');
+
+type $Without<T, K extends keyof any> = T extends any
+  ? Pick<T, Exclude<keyof T, K>>
+  : never;
+type $DeepPartial<T> = { [P in keyof T]?: $DeepPartial<T[P]> };
+
+export type ThemingType<Theme> = {
+  ThemeContext: React.Context<Theme>;
+  ThemeProvider: React.ComponentType<{
+    children: React.ReactNode;
+    theme?: Theme;
+  }>;
+  withTheme: <Props extends { theme: Theme }, C>(
+    WrappedComponent: React.ComponentType<Props> & C
+  ) => React.ComponentType<
+    $Without<Props, 'theme'> & { theme?: $DeepPartial<Theme> }
+  > &
+    hoistNonReactStatics.NonReactStatics<typeof WrappedComponent>;
+  useTheme<T = Theme>(overrides?: $DeepPartial<T>): T;
+};
+
+export const createTheming: <Theme>(defaultTheme: Theme) => ThemingType<Theme>;
+
+export { default as deepmerge } from 'deepmerge';
+
+export type { $DeepPartial as DeepPartial };
